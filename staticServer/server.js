a4523e8fbd5f35b57196f64492b21b6079dd47ff
Byron Lahey <nanozeit@gmail.com>
2021-09-27 09:16:58 -0700

    added dict unpack

--- staticServer/server.js
+++ staticServer/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 8082;
+var port = 8080;
 
 app.get("/sendData", function (req, res) {
   console.log(req.query);
