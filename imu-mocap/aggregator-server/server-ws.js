d2a432cf9b35fa8b094bc083e803388a8e25a114
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-10-26 07:51:27 -0700

    ac

--- imu-mocap/aggregator-server/server-ws.js
+++ imu-mocap/aggregator-server/server-ws.js
@@ -11,20 +11,9 @@ const wss = new WebSocket.Server({ port: 3000})
 wss.on('connection', ws => {
   ws.on('message', message => {
      console.log(`Received message => ${message}`)
-     try{
-      var x =  JSON.parse(`${message}`);
-      var euler = qte([parseFloat(req.query.w), parseFloat(req.query.x), parseFloat(req.query.y), parseFloat(req.query.z)]);
-      x.w = parseFloat(x.w);
-      x.x = parseFloat(x.x);
-      x.y = parseFloat(x.y);
-      x.z = parseFloat(x.z);
-      x.xE = euler[0]
-      x.yE = euler[1]
-      x.zE = euler[2]
-      values[req.query.id] = x
-     }
-     catch(e){
-     }
+     wss.clients.forEach(function each(client) {
+       client.send(`${message}`);
+     });
   })
   ws.send('start');
 })
--- imu-mocap/pod_mcu_code-08x-ws/pod_mcu_code-08x-ws.ino
+++ imu-mocap/pod_mcu_code-08x-ws/pod_mcu_code-08x-ws.ino
@@ -1,38 +1,91 @@
 
 
 #include <WiFi.h>
+#include <WiFiMulti.h>
 #include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <WebSocketsClient.h>
+
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
 
 
 #define LILYGO_WATCH_2019_WITH_TOUCH 
 #include <LilyGoWatch.h>
 TTGOClass *watch;
 TFT_eSPI *tft;
-BMA *sensor;
-String mac_address;
+
 
 #include <Wire.h>
+
 
 #include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
 BNO080 myIMU;
 
+const char* ssid = "NETGEAR31";
+const char* password = "fluffywind2904";
 
+String mac_address;
 
-const char* ssid = "AMEIoT";
-const char* password = "ameclass";
-
-//Your Domain name with URL path or IP address with path
-const char* serverName = "http://192.168.2.6:1234/sendData";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;   
+unsigned long lastTime = 0;
 // Timer set to 10 minutes (600000)
 //unsigned long timerDelay = 600000;
 // Set timer to 5 seconds (5000)
 unsigned long timerDelay = 10;
 
 String response;
+
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+  const uint8_t* src = (const uint8_t*) mem;
+  Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  for(uint32_t i = 0; i < len; i++) {
+    if(i % cols == 0) {
+      Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    Serial.printf("%02X ", *src);
+    src++;
+  }
+  Serial.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+      // send message to server when Connected
+      webSocket.sendTXT("Connected");
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send data to server
+      // webSocket.sendBIN(payload, length);
+      break;
+    case WStype_ERROR:      
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -49,25 +102,14 @@ void setup() {
     //Receive objects for easy writing
     tft = watch->tft;
 
+
+
     // Some display settings
     tft->setTextColor(random(0xFFFF));
     tft->drawString("BNO08x Quaternions",  5, 50, 4);
     tft->setTextFont(4);
     tft->setTextColor(TFT_WHITE, TFT_BLACK);
 
-
-      WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-  mac_address = WiFi.macAddress();
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 
 
   delay(100); //  Wait for BNO to boot
@@ -88,13 +130,52 @@ void setup() {
 
   Serial.println(F("Rotation vector enabled"));
   Serial.println(F("Output in form i, j, k, real, accuracy"));
+
+  delay(500);
+  
+   
+  WiFiMulti.addAP(ssid, password);
+  
+  Serial.println("Connecting");
+
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+
+  mac_address = WiFi.macAddress();
+  //mac_address = "test";
+  delay(500);
+  // server address, port and URL
+
+  webSocket.begin("192.168.0.193", 3000, "/");
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+
+  // use HTTP Basic Authorization this is optional remove if not needed
+  // webSocket.setAuthorization("user", "Password");
+
+  // try ever 5000 again if connection has failed
+  webSocket.setReconnectInterval(5000);
+
+    webSocket.sendTXT(String(millis()).c_str());
+
   
 }
 
 void loop() {
+    webSocket.loop();
  if ((millis() - lastTime) > timerDelay) {
     //Check WiFi connection status
     if(WiFi.status()== WL_CONNECTED){
+
 
   if (myIMU.dataAvailable() == true)
   {
@@ -122,17 +203,13 @@ void loop() {
       tft->setCursor(80, 190);
       tft->print("W:"); tft->println(quatReal);
          
-      String url = String(serverName) + "?x=" + quatI + "&y=" + quatJ + "&z=" + quatK + "&w=" + quatReal + "&id=" + macAddress; 
-      Serial.println(url);       
-      response = httpGETRequest(url.c_str());
-      Serial.println(response);
+
+      String url = "{\"id\": \"" + mac_address + "\",\"x\":" + quatI + ",\"y\":" + quatJ + ",\"z\":" + quatK + ",\"w\":" + quatReal + "}"; 
+      Serial.println(url);
+      webSocket.sendTXT(url.c_str());
 
   }
   
-      
-  
-
-      
 
 
     }
